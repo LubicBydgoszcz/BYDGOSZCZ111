07dc041b86924a0ade8f5979c76503a31e9e510b
FckPutinn <bekisz43@gmail.com>
2022-11-16 15:13:40 +0100

    dzialajacy program

--- login.php
+++ login.php
@@ -16,10 +16,11 @@
     </form>
     <?php
         if(isset($_REQUEST['login']) && isset($_REQUEST['password'])) {
+            require_once('config.php');
             require_once('class/User.class.php');
             $user = new User($_REQUEST['login'], $_REQUEST['password']);
             if($user->login()) {
-                echo "Zalogowano poprawnie".$user->getName();
+                echo "Zalogowano poprawnie: ".$user->getName();
             } else {
                 echo "Błędny login lub hasło";
             }
