d1d05cf6b4bdc119decbc7910c63a832bca463d2
FckPutinn <bekisz43@gmail.com>
2022-11-23 13:21:56 +0100

    twig działa super

--- config.php
+++ config.php
@@ -1,4 +1,9 @@
 <?php
+require_once('vendor/autoload.php');
+
+$loader = new Twig\Loader\FilesystemLoader('templates');
+$twig = new Twig\Environment($loader);
+
 $db = new mysqli('localhost', 'root', '', 'loginform');
 
 ?>
