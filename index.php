fc24d0c0a92c2f18f94278f525111357b588f58c
FckPutinn <bekisz43@gmail.com>
2022-11-30 13:29:32 +0100

    posta nie zapomnij

--- index.php
+++ index.php
@@ -14,10 +14,46 @@ Route::add('/login', function() {
     $twig->display('login.html.twig');
 });
 
+Route::add('/login', function() {
+    global $twig;
+    if(isset($_REQUEST['login']) && isset($_REQUEST['password'])) {
+        $user = new User($_REQUEST['login'], $_REQUEST['password']);
+        if($user->login()) {
+            $twig->display('message.html.twig', ['message' => "Zalogowano pomyślnie użytkownika"]);
+        } else {
+            $twig->display('login.html.twig', ['message' => "Błędny login lub hasło!"]);
+        }
+    }
+    else {
+        die("Nie otrzymano danych");
+    }
+}, 'post');
+
 Route::add('/register', function() {
     global $twig;
     $twig->display('register.html.twig');
 });
+
+Route::add('/register', function() {
+    global $twig;
+    if(isset($_REQUEST['login']) && isset($_REQUEST['password'])) {
+        if((empty($_REQUEST['login'])) || empty($_REQUEST['password']) || empty($_REQUEST['firstName']) || empty($_REQUEST['lastName'])) {
+            $twig->display('register.html.twig', ['message' => "Nie podano wymaganej wartości"]);
+            exit();
+        }
+        $user = new User($_REQUEST['login'], $_REQUEST['password']);
+        $user->setFirstName($_REQUEST['firstName']);
+        $user->setLastName($_REQUEST['lastName']);
+        if($user->register()) {
+            $twig->display('message.html.twig', ['message' => "Zarejestrowano poprawnie"]);
+        } else {
+            $twig->display('register.html.twig', ['message' => "Błąd w rejestracji użytkownika!"]);
+        }
+    }
+    else {
+        die("Nie otrzymano danych");
+    }
+}, 'post');
 
 Route::run('/BYDGOSZCZ111');
 ?>
